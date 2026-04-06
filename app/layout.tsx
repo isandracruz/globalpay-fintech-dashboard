186c60e8ac82d796e51fcd3a81512fa42067aab5
Isandra Cruz Diaz <isandra.cruz.2017@gmail.com>
2026-04-06 16:10:32 -0400

    chore: setup TanStack Query and DevTools
    - Installed @tanstack/react-query and @tanstack/react-query-devtools
    - Created React Query client provider for Next.js App Router
    - Wrapped root layout with Providers component

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+
 import "./globals.css";
+import ReactQueryProvider from "./providers/ReactQueryProvider";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -28,7 +30,11 @@ export default function RootLayout({
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
       suppressHydrationWarning
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className="min-h-full flex flex-col">
+        <ReactQueryProvider>
+          {children}
+        </ReactQueryProvider>
+      </body>
     </html>
   );
 }
