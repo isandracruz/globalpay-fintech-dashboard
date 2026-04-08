f7585b238b5491da15cb1738f1cc20152aaef1d8
Isandra Cruz Diaz <isandra.cruz.2017@gmail.com>
2026-04-08 11:41:11 -0400

    feat: implement dashboard layout with sidebar and header

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 
 import "./globals.css";
-import ReactQueryProvider from "./providers/ReactQueryProvider";
+import ReactQueryProvider from "../providers/ReactQueryProvider";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -25,12 +25,11 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html
-      lang="en"
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
-      suppressHydrationWarning
-    >
-      <body className="min-h-full flex flex-col">
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
+        suppressHydrationWarning
+      >
         <ReactQueryProvider>
           {children}
         </ReactQueryProvider>
